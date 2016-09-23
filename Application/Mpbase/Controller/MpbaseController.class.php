fab19d028df15491112223396b1a5bc9ffbaf0d5
liuyang87075853 <liuyang87075853@163.com>
2016-09-23 14:53:51 +0800

    新建Mpbase后台管理控制器

---
+++ Application/Mpbase/Controller/MpbaseController.class.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | Thinking [ 2016 - 09 - 23 14:50]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014-2015 http://tqtzx.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: s_洋仔 <liuyang87075853@163.com>
+// +----------------------------------------------------------------------
+
+
+namespace Admin\Controller;
+use Think\Controller;
+use Admin\Builder\AdminConfigBuilder;
+use Admin\Builder\AdminListBuilder;
+use Admin\Builder\AdminTreeListBuilder;
+
+/*
+ *
+ * 公众号后台管理逻辑
+ *
+ * */
+class MpbaseController extends  AdminController
+{
+
+}
