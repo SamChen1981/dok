1cae897f38d220665db2feed9b6c31ca2c1ba0f6
liuyang87075853 <liuyang87075853@163.com>
2016-09-24 16:22:37 +0800

    新增get_mpid方法获取当前公众号mpid

--- Application/Common/Common/weixin.php
+++ Application/Common/Common/weixin.php
@@ -19,3 +19,26 @@ function mpid_md5($str,$key=null){
     trace($str,'TplMsg2:','DEBUG',true);
     return ''===$str ? '' : md5(sha1($str).$key);
 }
+//获取上下文公众号的mp_id
+function get_mpid($mp_id=null){
+    if($mp_id !== null){
+        session('mp_id',$mp_id);
+    }else if(!empty($_REQUEST['mp_id'])){
+        $mp_id=I('mp_id','','/^\{32}$/');
+        empty($mp_id) || session('mp_id',$mp_id);
+    }
+    $mp_id=session('mp_id');
+    if(empty($mp_id)){
+        $mp_id=session('user_auth.mp_id');
+    }
+    if(empty($mp_id)){
+        $map['uid']=is_login();
+        $map['public_id']=get_token();
+        $mp=D('Mpbase/MemberPublic')->where($map)->find();//所登陆会员账号当前管理的公众号
+        $mp_id=$mp['mp_id'];
+    }
+    if(empty($mp_id)){
+        return -1;
+    }
+    return $mp_id;
+}
